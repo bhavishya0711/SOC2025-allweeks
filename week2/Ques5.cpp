121a9316931cd9342fd7370ef52c4a0c5522f335
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:34:55 +0530

    Update Ques5.cpp

--- week2/Ques5.cpp
+++ week2/Ques5.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
+int main(){
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    
+    for(int i=0; i<n; i++){
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    long long median = vec[n/2];
+
+    long long cost = 0;
+    for(auto &x : vec){
+        cost += abs(x - median);
+    }
+    cout << cost << "\n";
+    return 0;
+}
