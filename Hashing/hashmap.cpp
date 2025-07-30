9a864df4ed501027cc0f9d1266826cc97fb853d1
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:45:38 +0530

    Update hashmap.cpp

--- Hashing/hashmap.cpp
+++ Hashing/hashmap.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    unordered_map<int, int> mpp;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        mpp[arr[i]]++;
+    }
+
+//     for(auto x : mpp){
+//         cout << x.first << "->" << x.second << endl;
+//     }
+    int maxFreq = 0, minFreq = n;
+    int maxEle = 0, minEle = 0;
+
+    for(auto x : mpp){
+        int count = x.second;
+        int element = x.first;
+
+        if(count > maxFreq){
+            maxFreq = count;
+            maxEle = element;
+        }
+        if(count < minFreq){
+            minFreq = count;
+            minEle = element;
+        }
+    }
+    cout << maxEle << " " << minEle << "\n";
+}
