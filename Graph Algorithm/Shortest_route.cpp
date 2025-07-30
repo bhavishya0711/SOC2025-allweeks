edf5e39bbf9c43dd4c9266dce98dc0a8a681fb72
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:44:19 +0530

    Update Shortest_route.cpp

--- Graph Algorithm/Shortest_route.cpp
+++ Graph Algorithm/Shortest_route.cpp
@@ -1,1 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+const long long INF = 1e15;
+const int MAXN = 505;
+
+long long dist[MAXN][MAXN];
+
+int main() {
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    // Initialize distances
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j)
+            dist[i][j] = (i == j ? 0 : INF);
+
+    // Input edges
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+        dist[a][b] = min(dist[a][b], c);
+        dist[b][a] = min(dist[b][a], c); // since undirected
+    }
+
+    // Floyd-Warshall
+    for (int k = 1; k <= n; ++k)
+        for (int i = 1; i <= n; ++i)
+            for (int j = 1; j <= n; ++j)
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+
+    // Answer queries
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        if (dist[a][b] == INF) cout << -1 << "\n";
+        else cout << dist[a][b] << "\n";
+    }
+
+    return 0;
+}
 
