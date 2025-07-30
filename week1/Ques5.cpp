d4df26766f0bc76b8bf70fdef7753849dcde37ab
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:31:14 +0530

    Update Ques5.cpp

--- week1/Ques5.cpp
+++ week1/Ques5.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+  long long n;
+  cin >> n;
+  long long val = n;
+  cout<< val << " ";
+  
+  while(val!= 1){
+    if (val%2 == 0) {
+      val = val/2;
+      cout << val << " ";
+    }
+    else {
+      val = 3 * val + 1;
+      cout << val << " ";
+    }
+  }
+  return 0;
+}
