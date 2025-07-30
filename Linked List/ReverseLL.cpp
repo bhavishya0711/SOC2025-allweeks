9cab52c201869708634c3ca51bc5477f802206eb
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:48:55 +0530

    Update ReverseLL.cpp

--- Linked List/ReverseLL.cpp
+++ Linked List/ReverseLL.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* ReverseList(Node* head){
+    if(head == nullptr || head->next == nullptr){
+        return head;
+    }
+
+    Node* prev  = nullptr;
+    Node* curr = head;
+    while(curr != nullptr){
+        Node* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
+}
+
+int main(){
+    vector<int> arr = {8, 3, 4, 5};
+    
+}
