3f6cf0e1d3eb566900dd33532e6e477af25e9e76
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:57:39 +0530

    Update Power_Set.cpp

--- Power_Set.cpp
+++ Power_Set.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 
+void printF(int ind, vector<int> &ds, int arr[], int n){
+    if(ind == n){
+        for(auto it : ds){
+            cout << it << " ";
+        }
+        if(ds.empty()) cout << "{}";
+        cout << endl;
+        return;
+    }
+
+    printF(ind + 1, ds, arr, n);
+
+    ds.push_back(arr[ind]);
+    printF(ind + 1, ds, arr, n);
+    ds.pop_back();
+
+}
+
+int main(){
+    // int arr[] = {22, 53, 64};
+    // int n = 3;
+    // vector<int> ds;
+    // printF(0, ds, arr, n);
+
+    vector<int> arr = {0, 0, 1, 2, 2, 2 , 3, 4};
+    auto x = upper_bound(arr.begin(), arr.end(), 2);
+    auto y = lower_bound(arr.begin(), arr.end(), 2);
+    cout << x-arr.begin() <<" " << y-arr.begin();
+    return 0;
+}
