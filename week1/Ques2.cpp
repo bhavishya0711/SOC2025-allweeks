0b83421983ff5faf54ac1b29a5cea35fa04741f5
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:30:24 +0530

    Update Ques2.cpp

--- week1/Ques2.cpp
+++ week1/Ques2.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  string str;
+  cin >> str;
+  int times = 1;
+  int max_len = 1;
+  
+  for ( int  i= 1; i < str.size() ; i++){
+    if(str[i] == str[i-1]){
+      times += 1;
+      max_len = max(max_len, times);
+    }
+    else times = 1;
+  }
+  
+  cout << max_len;
+  return 0;
+}
