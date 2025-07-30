6694741f5cc6373e2224c1ea293bde7c91b79709
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:45:15 +0530

    Update hashing.cpp

--- Hashing/hashing.cpp
+++ Hashing/hashing.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int hash[13] = {0};
+    for(int i=0; i<n; i++){
+        hash[arr[i]] += 1;
+    }
+
+    int t;
+    cin >> t;
+    while(t--){
+        int number ;
+        cin >> number;
+        cout << hash[number] << "\n";
+    }
+}
