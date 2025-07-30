9ccd1852df14effc3dbf3517c90c7a89a8203ab7
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:46:01 +0530

    Update hashstring.cpp

--- Hashing/hashstring.cpp
+++ Hashing/hashstring.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    string s;
+    cin >> s;
+    
+    int hash[26] = {0};
+    for(int i= 0; i < s.size(); i++){
+        hash[s[i] - 'a'] += 1;
+    }
+
+    int t;
+    cin >> t;
+    while(t--){
+        char c;
+        cin >> c;
+        cout << hash[c - 'a'] << endl;
+    }
+    return 0;
+}
