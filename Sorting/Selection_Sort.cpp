9e878852a3b457a4f47ed8b53c76689b60fa2a30
Bhavishya Gupta <bhavishyagupta0711@gmail.com>
2025-07-30 14:51:51 +0530

    Update Selection_Sort.cpp

--- Sorting/Selection_Sort.cpp
+++ Sorting/Selection_Sort.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
 
+void selection_sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        int mini = i;
+        for (int j=i; j<n ; j++){
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+            swap(arr[i], arr[mini]);
+        }
+    } 
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    selection_sort(arr, n);
+    for (int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
